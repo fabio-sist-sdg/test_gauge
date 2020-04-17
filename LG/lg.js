d4474bbcc258672b6e202239e536a11040db6ab4
fabio-sist-sdg <55802391+fabio-sist-sdg@users.noreply.github.com>
2020-04-17 12:26:19 +0200

    added cors setting

--- LG/lg.js
+++ LG/lg.js
@@ -7905,7 +7905,7 @@
             }
         }
         loadLiquidFillGauge(t, e, a) {
-			fetch('https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02')
+			fetch('https://cors-anywhere.herokuapp.com/https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02')
 			.then( response => {
 				return response.json();
 			})
