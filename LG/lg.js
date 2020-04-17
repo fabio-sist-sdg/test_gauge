a192cdb559307650aeab161f634943fa2b6b82b8
fabio-sist-sdg <55802391+fabio-sist-sdg@users.noreply.github.com>
2020-04-17 12:07:23 +0200

    Update api call

--- LG/lg.js
+++ LG/lg.js
@@ -7905,7 +7905,7 @@
             }
         }
         loadLiquidFillGauge(t, e, a) {
-			fetch('api.openweathermap.org/data/2.5/weather?q=London')
+			fetch('https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02')
 			.then( response => {
 				return response.json();
 			})
